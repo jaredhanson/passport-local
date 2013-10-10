8d244499152a22cb526b0561a6657f76983ea569
Nicola Rizzo <nicola.rizzo@gmail.com>
2013-10-10 16:27:21 +0200

    unneeded function in ensureAdmin

--- examples/express3-mongoose-multiple-files/config/pass.js
+++ examples/express3-mongoose-multiple-files/config/pass.js
@@ -37,11 +37,9 @@ exports.ensureAuthenticated = function ensureAuthenticated(req, res, next) {
 // Check for admin middleware, this is unrelated to passport.js
 // You can delete this if you use different method to check for admins or don't need admins
 exports.ensureAdmin = function ensureAdmin(req, res, next) {
-    return function(req, res, next) {
-	console.log(req.user);
-        if(req.user && req.user.admin === true)
-            next();
-        else
-            res.send(403);
-    }
+  console.log(req.user);
+  if(req.user && req.user.admin === true)
+      next();
+  else
+      res.send(403);
 }
