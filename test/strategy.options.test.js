d9782cdafd067306a63f2d2a042342e78015ac42
Jumpei Ogawa <ogawa@growaspeople.org>
2015-03-08 22:18:14 +0900

    Add test for authenticate()'s options.params argument

--- test/strategy.options.test.js
+++ test/strategy.options.test.js
@@ -34,4 +34,34 @@ describe('Strategy', function() {
     });
   });
 
+  describe('handling an authentication request from non-Express framework: username and password parameters are given as optional arguments', function() {
+    var user
+    , info;
+
+    before(function(done) {
+      chai.passport(new Strategy({ passReqToCallback: true }, function(req, username, password, done) {
+        if (username === 'johndoe' && password === 'secret') {
+          return done(null, { id: '1234' }, { scope: 'read' });
+        }
+        return done(null, false);
+      }))
+        .success(function(u, i) {
+        user = u;
+        info = i;
+        done();
+      })
+        .authenticate({ params: { username: 'johndoe', password: 'secret' }});
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+
+  });
 });
