0ea32ebf8c1009965defe7536f27e426f02255de
Jumpei Ogawa <ogawa@growaspeople.org>
2015-03-08 21:21:43 +0900

    Remove trailing spaces

--- test/strategy.options.test.js
+++ test/strategy.options.test.js
@@ -6,14 +6,14 @@ var chai = require('chai')
 
 
 describe('Strategy', function() {
-    
+
   describe('handling a request without a body, but no username and password, with message option to authenticate', function() {
     var strategy = new Strategy(function(username, password, done) {
       throw new Error('should not be called');
     });
-    
+
     var info, status;
-    
+
     before(function(done) {
       chai.passport(strategy)
         .fail(function(i, s) {
@@ -26,12 +26,12 @@ describe('Strategy', function() {
         })
         .authenticate({ badRequestMessage: 'Something is wrong with this request' });
     });
-    
+
     it('should fail with info and status', function() {
       expect(info).to.be.an.object;
       expect(info.message).to.equal('Something is wrong with this request');
       expect(status).to.equal(400);
     });
   });
-  
+
 });
