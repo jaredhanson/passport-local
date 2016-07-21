0a158b3cdab96a69dbf70424e8c195753b3a1d0b
Stan Bondi <stan@fixate.it>
2016-07-21 14:56:48 +0200

    Allow definition of multiple username fields
    Usage:
    
    options.usernameFields = ['username', 'user[email]'];

--- test/strategy.fields.test.js
+++ test/strategy.fields.test.js
@@ -6,7 +6,7 @@ var chai = require('chai')
 
 
 describe('Strategy', function() {
-    
+
   describe('handling a request with valid credentials in body using custom field names', function() {
     var strategy = new Strategy({ usernameField: 'userid', passwordField: 'passwd' }, function(username, password, done) {
       if (username == 'johndoe' && password == 'secret') {
@@ -14,10 +14,10 @@ describe('Strategy', function() {
       }
       return done(null, false);
     });
-    
+
     var user
       , info;
-    
+
     before(function(done) {
       chai.passport(strategy)
         .success(function(u, i) {
@@ -32,18 +32,18 @@ describe('Strategy', function() {
         })
         .authenticate();
     });
-    
+
     it('should supply user', function() {
       expect(user).to.be.an.object;
       expect(user.id).to.equal('1234');
     });
-    
+
     it('should supply info', function() {
       expect(info).to.be.an.object;
       expect(info.scope).to.equal('read');
     });
   });
-  
+
   describe('handling a request with valid credentials in body using custom field names with object notation', function() {
     var strategy = new Strategy({ usernameField: 'user[username]', passwordField: 'user[password]' }, function(username, password, done) {
       if (username == 'johndoe' && password == 'secret') {
@@ -51,10 +51,10 @@ describe('Strategy', function() {
       }
       return done(null, false);
     });
-    
+
     var user
       , info;
-    
+
     before(function(done) {
       chai.passport(strategy)
         .success(function(u, i) {
@@ -70,16 +70,54 @@ describe('Strategy', function() {
         })
         .authenticate();
     });
-    
+
     it('should supply user', function() {
       expect(user).to.be.an.object;
       expect(user.id).to.equal('1234');
     });
-    
+
     it('should supply info', function() {
       expect(info).to.be.an.object;
       expect(info.scope).to.equal('read');
     });
   });
-  
+
+  describe('handling a request with valid credentials in body using custom multiple field names with object notation', function() {
+    var strategy = new Strategy({ usernameFields: ['user[username]', 'user[email]'], passwordField: 'user[password]' }, function(username, password, done) {
+      if (username == 'johndoe@test.com' && password == 'secret') {
+        return done(null, { id: '1234' }, { scope: 'read' });
+      }
+      return done(null, false);
+    });
+
+    var user
+      , info;
+
+    before(function(done) {
+      chai.passport(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.body = {};
+          req.body.user = {};
+          req.body.user.email = 'johndoe@test.com';
+          req.body.user.password = 'secret';
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
+  });
+
 });
